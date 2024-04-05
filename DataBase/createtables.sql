489d5ecb30266b000e4f0fa20e81402c670ca42e
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-05 13:19:03 -0500

    git

--- DataBase/createtables.sql
+++ DataBase/createtables.sql
@@ -7,3 +7,15 @@ CREATE TABLE IF NOT EXISTS planets (
     subjects JSONB,
     planets TEXT
 );
+
+-- Create subject_matter table
+CREATE TABLE IF NOT EXISTS planets (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    colorJSONB,
+    subjects JSONB,
+    planets TEXT
+
+
+
+)
