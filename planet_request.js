00ff7a49f2a4332765e1e98083acad9a9effaeee
Caaronbll <aaronbll46@gmail.com>
2024-04-05 11:24:42 -0500

    2 planets

--- planet_request.js
+++ planet_request.js
@@ -1,7 +1,22 @@
 const reques = require('request');
-var name = 'Neptune';
+const neptune = 'Neptune';
+const jupiter = 'Jupiter';
+
+//request for neptune
 reques.get({
-  url: 'https://api.api-ninjas.com/v1/planets?name=' + name,
+  url: 'https://api.api-ninjas.com/v1/planets?name=' + neptune,
+  headers: {
+    'X-Api-Key': 'LWAlV9HqT5NvybZq/V2oRQ==PG3xR4eLwJqp6NoE'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else console.log(body)
+});
+
+//request for jupiter
+reques.get({
+  url: 'https://api.api-ninjas.com/v1/planets?name=' + jupiter,
   headers: {
     'X-Api-Key': 'LWAlV9HqT5NvybZq/V2oRQ==PG3xR4eLwJqp6NoE'
   },
