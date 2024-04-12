da4e241433133f20803768865c4597577079fc49
Caaronbll <aaronbll46@gmail.com>
2024-04-12 11:10:27 -0500

    setting up database

---
+++ Server/DataBase/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+
+mongoDBAtlasUri = 'mongodb+srv://new_user:urgsQzpeKLFY1yWl@cluster0.xnrspad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+mongoose.connect(mongoDBAtlasUri, { useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+port = 2500;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
