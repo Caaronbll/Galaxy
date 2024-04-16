d884c0391a7b3f7f79e02651f46ef56de552a2dd
Caaronbll <aaronbll46@gmail.com>
2024-04-16 11:37:50 -0500

    database

--- Server/DataBase/app.js
+++ Server/DataBase/app.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const GalaxyModel = require('./Schema');
 
-mongoDBAtlasUri = 'mongodb+srv://new_user:urgsQzpeKLFY1yWl@cluster0.xnrspad.mongodb.net/Galaxy?retryWrites=true&w=majority&appName=Cluster0';
+mongoDBAtlasUri = 'mongodb+srv://new_user:urgsQzpeKLFY1yWl@cluster0.xnrspad.mongodb.net/new_galaxy?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(mongoDBAtlasUri, { useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 });
 
+const db = mongoose.connection.db;
+
 port = 2500;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
   });
+
+app.get('/galaxies', async (req, res) => {
+    try {
+    GalaxyModel.find()
+    .then(galaxies => {
+        res.json(galaxies);
+    })
+    .catch(err => {
+        res.status(400).json({ message: err.message });
+    })
+}
+catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
--- Server/DataBase/schema.js
+++ Server/DataBase/schema.js
@@ -3,14 +3,22 @@ const mongoose = require('mongoose');
 // Define the schema for the collection
 const galaxySchema = new mongoose.Schema({
   name: String,
-  type: String,
-  distanceFromEarth: Number,
-  diameter: Number,
-  numberOfStars: Number,
+  mass: Number,
+  radius: Number,
+  period: Number,
+  semi_major_axis: Number,
+  temperature: Number,
+  distance_light_year: Number,
+  host_star_mass: Number,
+  host_star_temperature: Number,
+  surface_area: Number,
+  Lots: Number,
+  Prices: Number
+  
 });
 
 // Create a model based on the schema
-const Galaxy = mongoose.model('Galaxy', galaxySchema);
+const GalaxyModel = mongoose.model('Galaxy', galaxySchema);
 
 // Export the model so it can be used in other files
-module.exports = Galaxy;
+module.exports = GalaxyModel;
