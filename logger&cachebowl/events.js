c924d9edbec3e96274b4c329e43a8350327329e3
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-18 12:50:46 -0500

    events

---
+++ logger&cachebowl/events.js
@@ -0,0 +1,9 @@
+// events.js
+
+module.exports = {
+    set: 'Fired when a key has been added or changed.',
+    del: 'Fired when a key has been removed manually or due to expiry.',
+    expired: 'Fired when a key expires.',
+    flush: 'Fired when the cache has been flushed.',
+    flush_stats: 'Fired when the cache stats has been flushed.'
+};
