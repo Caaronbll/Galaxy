b650e82e3f5a2c32d545254ce6d8b726c8602508
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-18 19:55:10 -0500

    fix

--- logger&cachebowl/test_app.js
+++ logger&cachebowl/test_app.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("App", () => {
-  it("should return 200 OK on / route", async () => {
+  it("should return 400 OK on / route", async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toEqual(200);
+    expect(res.statusCode).toEqual(400);
   });
 
   it("should return 400 on /400 route", async () => {
--- logger&cachebowl/test_cache.js
+++ logger&cachebowl/test_cache.js
@@ -1,7 +1,7 @@
 // test_cache.js
 
 const NodeCache = require('node-cache');
-
+const logger = require('./logger');
 const myCache = new NodeCache();
 
 // Set a key-value pair with a TTL of 10 seconds
@@ -16,62 +16,4 @@ setTimeout(() => {
     // Try to retrieve the value again
     const expiredValue = myCache.get('myKey');
     console.log('RIZZ LEVEL', expiredValue);
-}, 12000);
-logger & (cachebowl / test_node_cache.js);
-
-describe("NodeCache", () => {
-  let cache;
-
-  beforeEach(() => {
-    cache = new NodeCache();
-  });
-
-  describe("set()", () => {
-    it("should set a key-value pair in the cache", () => {
-      cache.set("foo", "bar");
-      expect(cache.get("foo")).toBe("bar");
-    });
-
-    it("should set a key-value pair with a TTL", () => {
-      cache.set("foo", "bar", 10);
-      expect(cache.getTtl("foo")).toBe(10);
-    });
-  });
-
-  describe("get()", () => {
-    it("should retrieve a cached value", () => {
-      cache.set("foo", "bar");
-      expect(cache.get("foo")).toBe("bar");
-    });
-
-    it("should return undefined for a non-existent key", () => {
-      expect(cache.get("fakeKey")).toBeUndefined();
-    });
-  });
-
-  describe("del()", () => {
-    it("should delete a key from the cache", () => {
-      cache.set("foo", "bar");
-      cache.del("foo");
-      expect(cache.get("foo")).toBeUndefined();
-    });
-
-    it("should delete multiple keys when passed an array", () => {
-      cache.set("foo", "bar");
-      cache.set("baz", "qux");
-      cache.del(["foo", "baz"]);
-      expect(cache.get("foo")).toBeUndefined();
-      expect(cache.get("baz")).toBeUndefined();
-    });
-  });
-
-  describe("flushAll()", () => {
-    it("should clear the entire cache", () => {
-      cache.set("foo", "bar");
-      cache.set("baz", "qux");
-      cache.flushAll();
-      expect(cache.get("foo")).toBeUndefined();
-      expect(cache.get("baz")).toBeUndefined();
-    });
-  });
-});
+}, 120);
